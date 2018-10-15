c3d9b8ab80f0e31997e78ec997e6c92ce68a5645
brunosemedo <43537229+brunosemedo@users.noreply.github.com>
2018-10-15 12:21:28 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,21 @@ namespace ConsoleApp1
         {
             //Coloque aqui o seu codigo
         
+  Console.WriteLine("insira o x");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("insira o Y");
+            double Y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > 0 && Y > 0)
+                Console.WriteLine("1º quadrante");
+            if (x < 0 && Y > 0)
+                Console.WriteLine("2º quadrante");
+            if (x < 0 && Y < 0)
+                Console.WriteLine("3º quadrante");
+            if (x > 0 && Y < 0)
+                Console.WriteLine("4º quadrante");
+            if (x == 0 && Y == 0)
+                Console.WriteLine("Origem do referencial");
         }
     }
 }
